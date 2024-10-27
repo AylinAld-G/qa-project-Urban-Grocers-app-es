3f174cffc52a17dcb73899758cc576d1005d88b2
Aylín Aldana <aylinaldanaguzman@gmail.com>
2024-10-26 22:43:04 -0600

    Update URL_SERVICE path

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-6442aced-8056-44de-982e-6d6c6e94c6d7.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-82f5b71b-d2f7-4223-9d5e-8a8f623a33d5.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
 DOC_PATH = "/docs/"
